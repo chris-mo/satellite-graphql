024fd407facc4e696f0210891bfdf4d70e98890e
chrismo <chrismo@zillowgroup.com>
2018-09-17 17:24:18 -0700

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import gqlServerConfig from './api';
 
 const serverOptions = {
-    port: 5000,
+    port: process.env.PORT || 5000,
     endpoint: '/graphql',
     playground: '/docs',
     tracing: true,
@@ -13,4 +13,3 @@ const serverOptions = {
 
 const server = new GraphQLServer(gqlServerConfig)
 server.start(serverOptions, ({port}) => console.log(`Server on port ${port}`))
-server.listen(process.env.PORT || 5000);
