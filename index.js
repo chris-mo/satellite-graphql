27f0d5b4600f268be4e86033655b30dfcf43ba98
chrismo <chrismo@zillowgroup.com>
2018-09-18 09:14:23 -0700

    use listen instead of start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import gqlServerConfig from './api';
 
 const serverOptions = {
-    port: process.env.PORT,
+    port: process.env.PORT || 5000,
     endpoint: '/graphql',
     playground: '/docs',
     tracing: true,
@@ -12,4 +12,6 @@ const serverOptions = {
   }
 
 const server = new GraphQLServer(gqlServerConfig)
-server.start(serverOptions, ({port}) => console.log(`Server on port ${port}`))
+server.listen(serverOptions, ({ port }) => {
+  console.log(`Server on port ${port}`)
+});
